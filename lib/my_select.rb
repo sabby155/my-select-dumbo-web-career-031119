33962efd16a8b2acf28a08dd94edcda1ce059794
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-12 23:25:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,7 @@ def my_select(collection)
   result = []
  i = 0 
  while i < collection.length 
-   if yield(collection[i])
-     result.push(collection[i])
-   end
+  result.push(collection[i]) if yield(collection[i])
  i += 1
  end
  result
